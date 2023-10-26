aac228ef5b1747301a01ece23b56b0eea7508635
mozerrr <newgener@tuta.io>
2023-10-26 17:39:32 +0300

    add aurora support

---
+++ examples/wonderous/aurora/flutter/generated_plugin_registrant.cpp
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include <flutter/application.h>
+#include <package_info_plus_aurora/package_info_plus_aurora_plugin.h>
+
+#include "generated_plugin_registrant.h"
+
+void RegisterPlugins() {
+    Application::RegisterPlugins({
+        std::make_shared<PackageInfoPlusAuroraPlugin>(),
+    });
+}
---
+++ examples/wonderous/aurora/flutter/generated_plugins.cmake
@@ -0,0 +1,36 @@
+#
+# Generated file, do not edit.
+#
+set(ROOT_PROJECT_BINARY_DIR "${PROJECT_BINARY_DIR}")
+
+function(add_library TARGET)
+    _add_library(${TARGET} ${ARGN})
+
+    if (
+      "${TARGET}" MATCHES "^package_info_plus_aurora_platform_plugin$" OR
+      "${TARGET}" MATCHES "^xdga_directories$" OR
+      FALSE
+    )
+      add_custom_command(TARGET ${TARGET} POST_BUILD
+                        COMMAND ${CMAKE_COMMAND} -E copy
+                        "$<TARGET_FILE:${TARGET}>"
+                        "${ROOT_PROJECT_BINARY_DIR}/bundle/lib/$<TARGET_FILE_NAME:${TARGET}>")
+    endif()
+endfunction()
+
+list(APPEND FLUTTER_PLATFORM_PLUGIN_LIST
+    package_info_plus_aurora
+)
+
+list(APPEND FLUTTER_FFI_PLUGIN_LIST
+    xdga_directories
+)
+
+foreach(PLUGIN ${FLUTTER_PLATFORM_PLUGIN_LIST})
+    add_subdirectory(flutter/ephemeral/.plugin_symlinks/${PLUGIN}/aurora plugins/${PLUGIN})
+    target_link_libraries(${BINARY_NAME} PRIVATE ${PLUGIN}_platform_plugin)
+endforeach(PLUGIN)
+
+foreach(FFI_PLUGIN ${FLUTTER_FFI_PLUGIN_LIST})
+    add_subdirectory(flutter/ephemeral/.plugin_symlinks/${FFI_PLUGIN}/aurora plugins/${FFI_PLUGIN})
+endforeach(FFI_PLUGIN)
